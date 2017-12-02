c2e3f67358e258c7e65a70a9f68cc353baf7df3c
Neous1 <Neous1@users.noreply.github.com>
2017-12-02 08:16:48 -0500

    create a class PersonData

--- CodeProjectScrape/Program.cs
+++ CodeProjectScrape/Program.cs
@@ -8,6 +8,21 @@ namespace CodeProjectScrape
 {
     class Program
     {
+        public class PersonData
+        {
+            public int ID { get; set; }
+            public string PersonName { get; set; }
+            public int Nationality { get; set; }
+
+            public PersonData(int id, int nationality ,string Name)
+            {
+                ID = id;
+                PersonName = Name;
+                Nationality = nationality;
+            }
+        }
+
+
         static void Main(string[] args)
         {
             //comments
